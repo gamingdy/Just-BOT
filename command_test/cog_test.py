d3f56e3a8614f4bcef13a25083fd2a1b5b2ab30d
Gamingdy <christgik12@gmail.com>
2022-04-27 14:54:45 +0200

    update cog_test.py
    Now only, get delay and user_name_discriminator from db,
    and new page system

--- command_test/cog_test.py
+++ command_test/cog_test.py
@@ -19,16 +19,13 @@ class TestCommand(commands.Cog):
         element = (
             self.db.cursor()
             .execute(
-                "SELECT * FROM slowmode_info WHERE channel_id = (?)  ORDER BY delay DESC",
+                "SELECT delay,user_name_discriminator FROM slowmode_info WHERE channel_id = (?) ORDER BY delay DESC",
                 (channel.id,),
             )
             .fetchall()
         )
         if element:
-            all_page = {}
-            for i in range(0, len(element), 5):
-                t = 1 if i == 0 else (i // 5) + 1
-                all_page[t] = element[i : i + 5]
+            all_page = [elem[i : i + 5] for i in range(0, len(elem), 5)]
 
             actual_page = 1
 
