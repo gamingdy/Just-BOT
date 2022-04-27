a65c895d040aa045fcf1262332b7e658614a93bb
Gamingdy <christgik12@gmail.com>
2022-04-27 23:21:31 +0200

    Lot of change, I don't know if it work

--- Utils/create_page.py
+++ Utils/create_page.py
@@ -1,3 +1,38 @@
-def generate_page(embed, field_name: list, field_value: list):
-    for name, value in zip(field_name, field_value):
+import discord
+
+
+def generate_page(embed, *args):
+    embed.clear_fields()
+    for value, name in args:
         embed.add_field(name=name, value=value, inline=False)
+
+
+class PageNavigation(discord.ui.View):
+    def __init__(self, nb_page, all_pages, embed):
+        super().__init__()
+        self.embed = embed
+        self.all_pages = all_pages
+        self.max_page = nb_page
+        self.actual_page = 0
+
+    @discord.ui.button(label="Next")
+    async def next_page(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
+        self.actual_page = (self.actual_page + 1) % self.max_page
+        generate_page(self.embed, *iter(self.all_pages[self.actual_page]))
+        self.embed.add_field(
+            name="Page", value=f"{self.actual_page + 1}/{self.max_page}", inline=False
+        )
+        await interaction.response.edit_message(embed=self.embed)
+
+    @discord.ui.button(label="Previous")
+    async def previous_page(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
+        self.actual_page = (self.actual_page - 1) % self.max_page
+        generate_page(self.embed, *iter(self.all_pages[self.actual_page]))
+        self.embed.add_field(
+            name="Page", value=f"{self.actual_page + 1}/{self.max_page}", inline=False
+        )
+        await interaction.response.edit_message(embed=self.embed)
--- command_test/cog_test.py
+++ command_test/cog_test.py
@@ -3,7 +3,8 @@ from discord.ext import commands
 from discord.commands import SlashCommandGroup
 import sqlite3
 
-from Utils.funct import create_embed, create_page
+from Utils.funct import create_embed
+from Utils.create_page import generate_page, PageNavigation
 
 
 class TestCommand(commands.Cog):
@@ -25,9 +26,21 @@ class TestCommand(commands.Cog):
             .fetchall()
         )
         if element:
-            all_page = [elem[i : i + 5] for i in range(0, len(elem), 5)]
+            all_page = [element[i : i + 5] for i in range(0, len(element), 5)]
+            my_embed = create_embed("Slowmode list")
+            all_page = list(
+                map(
+                    lambda l: list(map(lambda i: (f"Delay: {i[0]}", i[1]), l)),
+                    all_page,
+                )
+            )
 
-            actual_page = 1
+            generate_page(
+                my_embed, *iter(all_page[0])
+            )
+
+            my_navigation = PageNavigation(len(all_page), all_page, my_embed)
+            await ctx.respond(embed=my_embed, view=my_navigation)
 
 
 def setup(bot):
