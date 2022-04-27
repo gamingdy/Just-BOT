dfa80437428b3509fe9fe07bad78d264ddb48c9a
Gamingdy <christgik12@gmail.com>
2022-04-27 13:53:08 +0200

    add page in dict

--- command_test/cog_test.py
+++ command_test/cog_test.py
@@ -9,12 +9,28 @@ from Utils.funct import create_embed
 class TestCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.db = sqlite3.connect("data/bot_db.db")
 
     test_command = SlashCommandGroup("test", "Test command")
 
     @test_command.command()
     async def test(self, ctx):
-        print("oui")
+        channel = ctx.channel
+        element = (
+            self.db.cursor()
+            .execute(
+                "SELECT * FROM slowmode_info WHERE channel_id = (?)  ORDER BY delay DESC",
+                (channel.id,),
+            )
+            .fetchall()
+        )
+        if element:
+            all_page = {}
+            for i in range(0, len(element), 5):
+                t = 1 if i == 0 else (i // 5) + 1
+                all_page[t] = element[i : i + 5]
+
+            actual_page = 1
 
 
 def setup(bot):
