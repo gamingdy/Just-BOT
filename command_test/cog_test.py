a029a6ce6e3409fe6b0029e5447bc4dbccf62297
Gamingdy <christgik12@gmail.com>
2022-04-28 16:00:07 +0200

    add embed description in slowmode list command

--- command_test/cog_test.py
+++ command_test/cog_test.py
@@ -20,14 +20,16 @@ class TestCommand(commands.Cog):
         element = (
             self.db.cursor()
             .execute(
-                "SELECT delay,user_name_discriminator FROM slowmode_info WHERE channel_id = (?) ORDER BY delay DESC",
+                "SELECT delay,user_name_discriminator,channel_name FROM slowmode_info WHERE channel_id = (?) ORDER BY delay DESC",
                 (channel.id,),
             )
             .fetchall()
         )
+        my_embed = create_embed("Slowmode list")
         if element:
+            my_embed.description = f"*List of active slowmode in #{element[0][2]}*"
             all_page = [element[i : i + 5] for i in range(0, len(element), 5)]
-            my_embed = create_embed("Slowmode list")
+
             all_page = list(
                 map(
                     lambda l: list(map(lambda i: (f"Delay: {i[0]}", i[1]), l)),
@@ -36,10 +38,13 @@ class TestCommand(commands.Cog):
             )
 
             generate_page(my_embed, *iter(all_page[0]))
-            my_embed.set_footer(text=f"1/{len(all_page)}")
+            my_embed.set_footer(text=f"Page 1/{len(all_page)}")
 
             my_navigation = PageNavigation(len(all_page), all_page, my_embed)
             await ctx.respond(embed=my_embed, view=my_navigation)
+        else:
+            my_embed.description = "**No slowmode users in this channel**"
+            await ctx.respond(embed=my_embed)
 
 
 def setup(bot):
