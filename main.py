31391c965e798281eb4fdb92b0e3d94af901c33a
Gamingdy <christgik12@gmail.com>
2022-07-04 16:24:44 +0200

    I'm piece of shit
    This reverts commit c4e92e29b9d3fc13e520e1775b9ef7d079c4b57c.

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import discord
-from discord.ext import commands
 
 from config import TOKEN, debug_guild
 from Utils.funct import load_cog, verify_db, verify_user_slowmode
 
 statut = discord.Status.do_not_disturb
-bot = commands.Bot(
-    intents=discord.Intents.all(), status=statut, debug_guilds=debug_guild
-)
+bot = discord.Bot(intents=discord.Intents.all(), status=statut, debug_guilds=debug_guild)
 
 
 @bot.event
@@ -20,5 +17,5 @@ async def on_ready():
 
 verify_db()
 load_cog("commands", bot)
-# bot.load_extension("command_test.cog_test")
+bot.load_extension("command_test.cog_test")
 bot.run(TOKEN)
