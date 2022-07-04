9a0054220ec9d2ac0d1c25b58e798cd674e2e9fb
Gamingdy <christgik12@gmail.com>
2022-07-04 16:35:51 +0200

    deactivate load cog_test

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from config import TOKEN, debug_guild
 from Utils.funct import load_cog, verify_db, verify_user_slowmode
 
 statut = discord.Status.do_not_disturb
-bot = discord.Bot(intents=discord.Intents.all(), status=statut, debug_guilds=debug_guild)
+bot = discord.Bot(
+    intents=discord.Intents.all(), status=statut, debug_guilds=debug_guild
+)
 
 
 @bot.event
@@ -17,5 +19,5 @@ async def on_ready():
 
 verify_db()
 load_cog("commands", bot)
-bot.load_extension("command_test.cog_test")
+# bot.load_extension("command_test.cog_test")
 bot.run(TOKEN)
