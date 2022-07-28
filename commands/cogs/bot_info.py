a46f0453a6f8811d0862e6b65469dcf7f1403d1e
Gamingdy <christgik12@gmail.com>
2022-07-28 14:27:08 +0200

    move ui command in useful.py

--- commands/cogs/bot_info.py
+++ commands/cogs/bot_info.py
@@ -1,4 +1,3 @@
-import discord
 from discord.ext import commands
 from discord.commands import slash_command, option
 from datetime import datetime
@@ -80,68 +79,6 @@ class BotInfo(commands.Cog):
         )
 
         await ctx.respond(embed=message_embed)
-
-    @slash_command(description="Display information about a user or the command author")
-    async def ui(self, ctx, member: discord.Member = None):
-        user_id = member.id if member else ctx.author.id
-        user = ctx.guild.get_member(user_id)
-
-        fetch_user = await self.bot.fetch_user(user_id)
-        user_color = fetch_user.accent_color
-
-        username = "{}#{}".format(user.name, user.discriminator)
-        nickname = user.display_name
-        status_emoji = {"online": "🟢", "offline": "🔴", "dnd": "⛔", "idle": "🌙"}
-
-        status = user.status.value
-        if status in status_emoji:
-            status = f"{status_emoji[status]} {status}"
-        avatar_url = user.display_avatar.url
-        mutual_guild = len(user.mutual_guilds)
-
-        join_date = user.joined_at
-        date_joined = join_date.date().strftime("%Y/%m/%d")
-        time_joined = join_date.time().strftime("%H:%M:%S")
-
-        create_date = user.created_at
-        date_created = create_date.date().strftime("%Y/%m/%d")
-        time_created = create_date.time().strftime("%H:%M:%S")
-
-        activity = user.activity
-        if activity:
-            activity_name = activity.name
-            if isinstance(activity, discord.activity.CustomActivity):
-                activity = activity_name
-            elif isinstance(activity, discord.activity.Game):
-                activity = f"Playing {activity_name}"
-            elif isinstance(activity, discord.activity.Streaming):
-                activity = f"Streaming {activity_name} on {activity.platform}, [stream url]({activity.url})"
-            elif isinstance(activity, discord.activity.Spotify):
-                activity = f"Listening {activity.title}({activity.duration}), [track url]({activity.track_url})"
-        else:
-            activity = "Nothing"
-
-        separator = "-" * 20
-        embed_description = (
-            f"**Username** | {username}",
-            f"**Nickname** | {nickname}",
-            f"**Status** | {status}",
-            f"**Activity** | {activity}",
-            separator,
-            f"**Joined** | {date_joined} at {time_joined}",
-            f"**Created** | {date_created} at {time_created}",
-            separator,
-            f"**Common Guild** | {mutual_guild}",
-        )
-        embed_description = "\n".join(embed_description)
-        ui_embed = fonction.create_embed(
-            title=nickname,
-            thumbnail=avatar_url,
-            description=embed_description,
-            color=user_color,
-        )
-
-        await ctx.respond(embed=ui_embed)
 
 
 def setup(bot):
