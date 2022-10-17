897d463ae43063931e92c2fb7152fd18c2ce6ac5
Gamingdy <christgik12@gmail.com>
2022-10-17 14:38:02 +0200

    Create voice command group

--- commands/cogs/auto_voice.py
+++ commands/cogs/auto_voice.py
@@ -1,6 +1,18 @@
+from discord.commands import SlashCommandGroup
 from discord.ext import commands
+import discord
+
+
 class AutoVoice(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    auto_voice = SlashCommandGroup(name="voice", description="Auto voice command")
+
+    @auto_voice.command(description="Config auto voice channel")
+    async def config(self, ctx, channel_name):
+        ...
+
+
 def setup(bot):
     bot.add_cog(AutoVoice(bot))
