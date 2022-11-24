1d4ab705c21790f6f21064bc1f97d4191ed2465f
Gamingdy <christgik12@gmail.com>
2022-11-24 11:57:47 +0100

    Create extension.json, when the bot has started to store the list of extensions

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ with open("config.json") as conf:
     TOKEN = config_file["TOKEN"]
     database = sqlite3.connect(config_file["database"])
     debug_guild = config_file["debug"][1] if config_file["debug"][0] else None
+
+with open("data/extension.json", "w") as ext_list:
+    json.dump({}, ext_list, indent=4)
