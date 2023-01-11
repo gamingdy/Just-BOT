db085a086ca1f4b48d253e11ee6c0443cae801c3
Gamingdy <christgik12@gmail.com>
2023-01-11 10:40:58 +0100

    Add check in blocklist and whitelist command
    Check if user is already in whitelist or blocklist  to prevent duplication and permissions problem

--- commands/cogs/auto_voice.py
+++ commands/cogs/auto_voice.py
@@ -95,37 +95,98 @@ class AutoVoice(commands.Cog):
     @commands.check(connected_admin)
     async def block(self, ctx, user: discord.Member):
         user_id = user.id
-        voice_channel = ctx.author.voice.channel.id
+        voice_channel = ctx.author.voice.channel
+
         cursor = database.cursor()
-        cursor.execute(
-            "INSERT INTO blocklist (channel_id, user_id) VALUES (?,?) ",
+        is_blocked = cursor.execute(
+            "SELECT * FROM blocklist WHERE channel_id=(?) AND user_id=(?)",
             (
-                voice_channel,
+                voice_channel.id,
                 user_id,
             ),
+        ).fetchone()
+        if is_blocked:
+            message = "User is already blocked and can't be blocked twice 😀"
+        else:
+            is_whitelisted = cursor.execute(
+                "SELECT * FROM whitelist WHERE channel_id=(?) AND user_id=(?)",
+                (
+                    voice_channel.id,
+                    user_id,
+                ),
+            ).fetchone()
+            warn = ""
+            if is_whitelisted:
+                cursor.execute(
+                    "DELETE FROM whitelist WHERE channel_id=(?) AND user_id=(?)",
+                    (
+                        voice_channel.id,
+                        user_id,
+                    ),
+                )
+                warn = ":warning: **The user was whitelisted and has been removed**"
+
+            cursor.execute(
+                "INSERT INTO blocklist (channel_id, user_id) VALUES (?,?) ",
+                (
+                    voice_channel.id,
+                    user_id,
+                ),
+            )
+            database.commit()
+            message = f"{warn}\n\n{user.mention} is now blocklisted in channel <#{voice_channel.id}>"
+        embed = create_embed(
+            title="Blocklist update", description=message, color=discord.Color.red()
         )
-        database.commit()
-        await ctx.respond(
-            f"{user.mention} is now blocklisted in channel <#{voice_channel}>"
-        )
+        await ctx.respond(embed=embed)
 
     @auto_voice.command(description="Add user to voice channel white list")
     @commands.check(connected_admin)
     async def whitelist(self, ctx, user: discord.Member):
         user_id = user.id
-        voice_channel = ctx.author.voice.channel.id
+        voice_channel = ctx.author.voice.channel
+
         cursor = database.cursor()
-        cursor.execute(
-            "INSERT INTO whitelist (channel_id, user_id) VALUES (?,?) ",
+        is_whitelisted = cursor.execute(
+            "SELECT * FROM whitelist WHERE channel_id=(?) AND user_id=(?)",
             (
-                voice_channel,
+                voice_channel.id,
                 user_id,
             ),
-        )
-        database.commit()
-        await ctx.respond(
-            f"{user.mention} is now whitelisted in channel <#{voice_channel}>"
-        )
+        ).fetchone()
+        if is_whitelisted:
+            message = "User is already whitelisted"
+        else:
+            is_blocked = cursor.execute(
+                "SELECT * FROM blocklist WHERE channel_id=(?) AND user_id=(?)",
+                (
+                    voice_channel.id,
+                    user_id,
+                ),
+            ).fetchone()
+            warn = ""
+            if is_blocked:
+                cursor.execute(
+                    "DELETE FROM blocklist WHERE channel_id=(?) AND user_id=(?)",
+                    (
+                        voice_channel.id,
+                        user_id,
+                    ),
+                )
+                warn = ":warning: **The user was blocklisted and has been removed**"
+
+            cursor.execute(
+                "INSERT INTO whitelist (channel_id, user_id) VALUES (?,?) ",
+                (
+                    voice_channel.id,
+                    user_id,
+                ),
+            )
+            database.commit()
+            message = f"{warn}\n\n{user.mention} is now whitelisted in channel <#{voice_channel.id}>"
+        color = discord.Colour.from_rgb(255, 128, 243)
+        embed = create_embed(title="Whitelist update", description=message, color=color)
+        await ctx.respond(embed=embed)
 
 
 def setup(bot):
